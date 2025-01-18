4d2d5cc69d6ac217a74fe20fc7c85854fa82f23d
ssafarveisi <sajad.safarveisi@ionos.com>
2025-01-18 02:42:18 +0100

    Refactoring

--- setup-env.sh
+++ setup-env.sh
@@ -1,13 +1,10 @@
 # Remove the virtual env created by devbox (after running `devbox shell`) 
 rm -rf .venv/
 
-# Get the required python version in .python-version (e.g., 3.9.1)
+# Get the required python version in .python-version (already installed via pyenv)
 PYTHON_VERSION=$(pyenv version-name)
 
-# # Install the python version if not present
-pyenv install --skip-existing
-
-# Configure poetry to use the installed python version
+# Configure poetry to use the installed python version and create a virtual env again
 poetry env use $HOME/.pyenv/versions/$PYTHON_VERSION/bin/python
 poetry config virtualenvs.in-project true
 poetry check
