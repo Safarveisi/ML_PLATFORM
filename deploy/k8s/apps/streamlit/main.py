b9e796c529ab572c3bcee3433cff66112be0d073
Safarveisi <sajad.safarveisi@gmail.com>
2025-01-20 17:20:21 +0100

    Adding bells and whistles to streamlit

--- deploy/k8s/apps/streamlit/main.py
+++ deploy/k8s/apps/streamlit/main.py
@@ -1,23 +1,29 @@
+from datetime import timedelta
 from postgres_conn import PostgresHandler
 import streamlit as st
 
 st.title("API Usage Dashboard")
-st.markdown("""
-    <style>
-    .main .block-container {
-        max-width: 800px;
-        padding: 2rem;
-        padding-top: 5rem;
-    }
-    </style>
-    """, unsafe_allow_html=True)
 
 postgres_handler = PostgresHandler()
 df = postgres_handler.fetch_data()
 
-st.subheader("Data Sample")
-st.dataframe(df.head()) 
+DATE_FMT = "%Y-%m-%d"
+start_time = st.sidebar.date_input("Start date").strftime(DATE_FMT)
+end_time = (st.sidebar.date_input("End date") + timedelta(days=1)).strftime(DATE_FMT)
 
-st.subheader("APIs Overview")
-api_counts = df['api_name'].value_counts()
-st.bar_chart(api_counts)
+df_filter = df[df["created_at"].between(start_time, end_time, inclusive="both")] 
+
+if df_filter.empty:
+    st.write("There are no requests for the selected timeframe. :sleeping:")
+else:
+    st.subheader("Data Sample")
+    st.dataframe(
+        df_filter,
+        height=(len(df_filter) + 1) * 30,
+        use_container_width=True,
+        hide_index=True
+    )
+    st.divider()
+    st.subheader("APIs Overview")
+    api_counts = df_filter['api_name'].value_counts()
+    st.bar_chart(api_counts, horizontal=True, use_container_width=True)
