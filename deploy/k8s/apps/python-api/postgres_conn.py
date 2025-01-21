979b32b31960a195b3f2ed4b6200ab60e47890f2
Safarveisi <sajad.safarveisi@gmail.com>
2025-01-21 23:03:57 +0100

    Adding doc to methods

--- deploy/k8s/apps/python-api/postgres_conn.py
+++ deploy/k8s/apps/python-api/postgres_conn.py
@@ -8,6 +8,7 @@ class PostgresHandler:
         self.db_url = os.environ["DATABASE_URL"]
 
     def insert_into_postgres_table(self, api_name: str) -> None:
+        '''Inserts a row in a postgres table'''
         with psycopg.connect(self.db_url) as conn:
             with conn.cursor() as cur:
                 query = f"""INSERT INTO public.request (api_name)
