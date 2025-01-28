ccc514272a8d5a1ea9e35969a52b3acf85ea1ab1
Safarveisi <sajad.safarveisi@gmail.com>
2025-01-28 21:22:36 +0100

    Cross validation with MLlib

--- data_platform/script.py
+++ data_platform/script.py
@@ -1,25 +1,64 @@
-import mlflow
-from mlflow.entities import ViewType
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, when
+
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.feature import HashingTF, Tokenizer
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 
 spark = SparkSession.builder.appName("MLPlatform").getOrCreate()
-df = spark.createDataFrame(
-    [
-        ("sue", 32),
-        ("li", 3),
-        ("bob", 75),
-        ("heo", 13),
-    ],
-    ["first_name", "age"],
-)
-df1 = df.withColumn(
-    "life_stage",
-    when(col("age") < 13, "child")
-    .when(col("age").between(13, 19), "teenager")
-    .otherwise("adult"),
-)
-df1.write.parquet("s3a://customerintelligence/ml_platform/test", mode="overwrite")
 
-mlflow.set_tracking_uri("http://ip:5000")
-print(f"The Mlflow tracking uri is: {mlflow.get_tracking_uri()}")
+training = spark.createDataFrame([
+    (0, "a b c d e spark", 1.0),
+    (1, "b d", 0.0),
+    (2, "spark f g h", 1.0),
+    (3, "hadoop mapreduce", 0.0),
+    (4, "b spark who", 1.0),
+    (5, "g d a y", 0.0),
+    (6, "spark fly", 1.0),
+    (7, "was mapreduce", 0.0),
+    (8, "e spark program", 1.0),
+    (9, "a e c l", 0.0),
+    (10, "spark compile", 1.0),
+    (11, "hadoop software", 0.0)
+], ["id", "text", "label"])
+
+# Configure an ML pipeline, which consists of tree stages: tokenizer, hashingTF, and lr.
+tokenizer = Tokenizer(inputCol="text", outputCol="words")
+hashingTF = HashingTF(inputCol=tokenizer.getOutputCol(), outputCol="features")
+lr = LogisticRegression(maxIter=10)
+pipeline = Pipeline(stages=[tokenizer, hashingTF, lr])
+
+# We now treat the Pipeline as an Estimator, wrapping it in a CrossValidator instance.
+# This will allow us to jointly choose parameters for all Pipeline stages.
+# A CrossValidator requires an Estimator, a set of Estimator ParamMaps, and an Evaluator.
+paramGrid = ParamGridBuilder() \
+    .addGrid(hashingTF.numFeatures, [10, 100, 1000]) \
+    .addGrid(lr.regParam, [0.1, 0.01]) \
+    .build()
+
+crossval = CrossValidator(estimator=pipeline,
+                          estimatorParamMaps=paramGrid,
+                          evaluator=BinaryClassificationEvaluator(),
+                          numFolds=3)
+
+# Run cross-validation, and choose the best set of parameters.
+cvModel = crossval.fit(training)
+
+# Prepare test documents, which are unlabeled.
+test = spark.createDataFrame([
+    (4, "spark i j k"),
+    (5, "l m n"),
+    (6, "mapreduce spark"),
+    (7, "apache hadoop")
+], ["id", "text"])
+
+# Make predictions on test documents. cvModel uses the best model found (lrModel).
+prediction = cvModel.transform(test)
+selected = prediction.select("id", "text", "probability", "prediction")
+
+# Write the predictions into S3
+selected.write.parquet("s3a://customerintelligence/ml_platform/predictions", mode="overwrite")
+
+for row in selected.collect():
+    print(row)
