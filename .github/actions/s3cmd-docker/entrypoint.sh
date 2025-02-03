2898cfda70915a2104292ad7ff6d1bbb8423ba96
Safarveisi <sajad.safarveisi@gmail.com>
2025-02-04 00:55:17 +0100

    docker action

--- .github/actions/s3cmd-docker/entrypoint.sh
+++ .github/actions/s3cmd-docker/entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh -l
 
-S3CMD_PATH=/opt/s3cmd/s3cmd
+set -e
+
 S3CMD_CONFIG=/github/home/.s3cfg
 
 COMMAND=$1
 ACCESS_KEY=$2
 SECRET_KEY=$3
-ENDPOINT_URL=$4
+HOST_BASE=$4
 BUCKET_LOCATION=$5
 
-echo "$ACCESS_KEY"
-
-echo "access_key=$ACCESS_KEY" >> $S3CMD_CONFIG
-echo "secret_key=$SECRET_KEY" >> $S3CMD_CONFIG
-echo "host_base=$ENDPOINT_URL" >> $S3CMD_CONFIG
-echo "bucket_location=$BUCKET_LOCATION" >> $S3CMD_CONFIG
+sed -i s\|{{BUCKET_LOCATION}}\|${BUCKET_LOCATION}\|g ${S3CMD_CONFIG}
+sed -i s\|{{HOST_BASE}}\|${HOST_BASE}\|g ${S3CMD_CONFIG}
+sed -i s\|{{ACCESS_KEY}}\|${ACCESS_KEY}\|g ${S3CMD_CONFIG}
+sed -i s\|{{SECRET_KEY}}\|${SECRET_KEY}\|g ${S3CMD_CONFIG}
 
 # Run the user-provided s3cmd command (e.g., 'put file.txt s3://my-bucket')
-${S3CMD_PATH} $COMMAND
+s3cmd ${COMMAND}
+
+rm /github/home/.s3cfg
