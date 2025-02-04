d9b2e3d8a4bab97a1c36e1fcd55b0e19122ac6bb
Safarveisi <sajad.safarveisi@gmail.com>
2025-02-04 01:54:14 +0100

    docker action

--- .github/actions/s3cmd-docker/entrypoint.sh
+++ .github/actions/s3cmd-docker/entrypoint.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-S3CMD_PATH=/opt/s3cmd/s3cmd
-
 cp /.s3cfg /github/home/.s3cfg
 
 COMMAND=$1
@@ -18,6 +16,6 @@ sed -i s\|{{ACCESS_KEY}}\|${ACCESS_KEY}\|g /github/home/.s3cfg
 sed -i s\|{{SECRET_KEY}}\|${SECRET_KEY}\|g /github/home/.s3cfg
 
 # Run the user-provided s3cmd command (e.g., 'put file.txt s3://my-bucket')
-${S3CMD_PATH} ${COMMAND}
+s3cmd ${COMMAND}
 
 rm /github/home/.s3cfg
