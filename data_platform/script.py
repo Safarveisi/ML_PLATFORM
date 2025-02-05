ae3f6f11dc6452a1033909142ef55e40a1231c77
Safarveisi <sajad.safarveisi@gmail.com>
2025-02-05 13:00:44 +0100

    Multiple csv in the destination

--- data_platform/script.py
+++ data_platform/script.py
@@ -3,7 +3,7 @@ from pyspark.sql.functions import col, when
 
 spark = SparkSession.builder.appName("MLPlatform").getOrCreate()
 
-# Processing raw Iris data
+# Processing raw Iris data (${S3_BUCKET} will be filled via envsubst later - see Taskfile.yml)
 df = spark.read.csv(
     "s3a://${S3_BUCKET}/ml_platform/etl/raw/iris.data",
     inferSchema=True,
@@ -16,5 +16,5 @@ df1 = df.withColumn("target", when(col("target") == "Iris-setosa", 0)
                              .when(col("target") == "Iris-versicolor", 1)
                              .when(col("target") == "Iris-virginica", 2))
 
-# Write the preprocessed spark df into S3 as a single csv file (${S3_BUCKET} will be filled via envsubst later - see Taskfile.yml)
-df1.coalesce(1).write.format("csv").option("header", "true").mode("overwrite").save("s3a://${S3_BUCKET}/ml_platform/etl/preprocessed")
+# Write the preprocessed spark df into S3 as a single csv file
+df1.write.format("csv").option("header", "true").mode("overwrite").save("s3a://${S3_BUCKET}/ml_platform/etl/preprocessed")
